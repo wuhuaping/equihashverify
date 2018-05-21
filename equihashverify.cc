3b86f0a3ac40298a891c81a7c774df46a052bc98
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2018-05-21 23:16:52 +0300

    Changed verification params to 144/5 in main file

--- equihashverify.cc
+++ equihashverify.cc
@@ -10,8 +10,8 @@
 using namespace v8;
 
 int verifyEH(const char *hdr, const std::vector<unsigned char> &soln){
-  unsigned int n = 200;
-  unsigned int k = 9;
+  unsigned int n = 144;
+  unsigned int k = 5;
 
   // Hash state
   crypto_generichash_blake2b_state state;
@@ -19,7 +19,7 @@ int verifyEH(const char *hdr, const std::vector<unsigned char> &soln){
 
   crypto_generichash_blake2b_update(&state, (const unsigned char*)hdr, 140);
 
-  bool isValid = Eh200_9.IsValidSolution(state, soln);
+  bool isValid = Eh144_5.IsValidSolution(state, soln);
   
   return isValid;
 }
